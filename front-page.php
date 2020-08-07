2e31d5f93f0e32f747507c857938339943d186fa
Gintare Simutyte <gintare.simutyte@orca-affairs.de>
2020-08-07 10:13:53 +0200

    cover logo

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<div class="cover-page">
+  <div class="cover-page--font">
+            Shared History
+  </div> 
+</div>
+
+<script>
+
+setTimeout(function(){ 
+  window.location.href = '<?php echo esc_url( get_permalink(114)); ?>'; 
+}, 2000);
+
+</script>
+
+
+<?php get_footer(); ?>
